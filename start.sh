c831f68b76307ea6e581854b68dbbca7a86cbe8c
acturus <viktorsavkin333@gmail.com>
2025-04-14 21:26:15 +0300

    Добавил презентацию и README-файл

--- start.sh
+++ start.sh
@@ -1,29 +1,14 @@
 #!/bin/bash
 
-exec > /dev/null 2>&1
-
 TOKEN="$1"
 
-mkdir log
+mkdir log > tmp.txt
 
-python -m venv .venv
+python -m venv .venv > tmp.txt
 source .venv/bin/activate
-.venv/bin/python .venv/bin/pip install -r requirements.txt
+.venv/bin/python .venv/bin/pip install -r requirements.txt > tmp.txt
 
 export FLASK_APP=main.py
-
-flask shell -c "
-from app import db
-db.create_all()
-"
-
-if command -v apt &> /dev/null; then
-    sudo apt install -y pandoc
-elif command -v dnf &> /dev/null; then
-    sudo dnf install -y pandoc
-elif command -v pacman &> /dev/null; then
-    sudo pacman -S --noconfirm pandoc
-fi
 
 .venv/bin/python main.py > log/flask.log 2>&1 &
 
@@ -32,7 +17,8 @@ export TOKEN
 
 stop_scripts() {
     pkill -f ".venv/bin/python main.py"
-    pkill -f ".venv/bin/python bot.py"    
+    pkill -f ".venv/bin/python bot.py"
+    echo 'Процесс завершен'
     exit
 }
 
